61a08285d6fa123d4f605453f270cdaaad359a1a
Devin Nelson <devin.nelson@colostate.edu>
2018-12-03 11:27:49 -0700

    Attempt to fix slider

--- Shiny Web App/server.R
+++ Shiny Web App/server.R
@@ -3,11 +3,12 @@ library(dplyr)
 library(lubridate)
 source("helper.R")
 
-california_counties <- readRDS("../ca_county_cases_final.RDS") %>% 
-  mutate(date = ymd(date, tz = "America/Los_Angeles"))
+california_counties <- readRDS("../ca_county_cases_final.RDS") 
+california_counties$date <- ymd(california_counties$date, tz = "America/Los_Angeles")
 
 shinyServer(function(input, output) {
-  output$california_map <- renderPlot({plot_map()})
+  output$california_map <- renderPlot({plot_map()
+    })
 })
   
   
--- Shiny Web App/ui.R
+++ Shiny Web App/ui.R
@@ -1,18 +1,19 @@
-library(shiny)
-
 shinyUI(fluidPage(
-  titlePanel("CA Counties West Niles Virus Cases and Precipitation Averages 2006-2010"),
+
+  titlePanel("California Maps"),
+  
   sidebarLayout(position = "right",
-                sidebarPanel("Date",
+                sidebarPanel("Choose what month and year to display",
                              sliderInput(inputId = "time_range",
                                          label = "Select the time range: ",
-                                         value = c(as.POSIXct("2006-01-01"),
-                                                   as.POSIXct("2010-12-31")),
-                                         min = as.POSIXct("2006-01-01"),
-                                         max = as.POSIXct("2010-12-31"))),
+                                         value = c(as.Date("2006-01-01",
+                                                           tz = "America/Los_Angeles",
+                                                           timeFormat="%m %Y")),
+                                         min = as.Date("2006-01-01", tz = "America/Los_Angeles"),
+                                         max = as.Date("2010-12-01", tz = "America/Los_Angeles"),
+                                         step = 31)),
                 mainPanel("Map of California",
-                plotOutput("california_map"))                
+                          plotOutput("california_map"))
   )
+  
 ))
-
-
