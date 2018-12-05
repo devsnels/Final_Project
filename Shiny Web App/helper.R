a2e0f17ead35046c3a36cec4267fb8c1e9071d8b
Anastasia Ratcliff <aratclif@colostate.edu>
2018-12-05 11:13:40 -0700

    Changing pathway

--- Shiny Web App/helper.R
+++ Shiny Web App/helper.R
@@ -1,4 +1,4 @@
-load("~/Desktop/Final_Project/Data/ca_county_cases_final.RData")
+load("/Users/anastasiaratcliff/Desktop/R- Programing /Final_Project/Data/ca_county_cases_final.RData")
 
 plot_map <- function(df = ca_county_cases,
                      start.date = 2006,
